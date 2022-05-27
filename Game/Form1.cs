e08d0c94015bd60d3fc71d9e05307167a72ddc18
Коршунов Никита <nikita-ko85@mail.ru>
2022-05-28 03:37:55 +0500

    Добавил 2 класса и свойство Need, добавил событие на button16, добавил 2 метода GameTwo, GameThree

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -15,6 +15,9 @@ namespace Game
     public partial class Form1 : Form
     {
         Game1 game1;
+        Game2 game2; //Никита1
+        Game3 game3; //Никита1
+        public bool Need { get; set; }
         public Form1()
         {
             InitializeComponent();
@@ -26,6 +29,13 @@ namespace Game
             game1 = new Game1(this);
         }
         public void button0_Click(object sender, EventArgs e) => game1.button0_Click(sender, e);
+        public void button16_Click(object sender, EventArgs e) => Need = true; 
+
+        public void GameOne() => game1.StartGame();
+
+        public void GameTwo() => game2.StartGame();
+        public void GameThree() => game3.StartGame();
+
 
     }
 }
